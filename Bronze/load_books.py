5b1afc87eabd7d1f0b4f47d63c8af2eafb34a6a5
andre-ls <andre.luis.e3@gmail.com>
2022-11-20 11:04:51 -0300

    Added Env Variable for Data Directory

--- Bronze/load_books.py
+++ Bronze/load_books.py
@@ -1,11 +1,15 @@
+from dotenv import load_dotenv
+import os
+import json
 from pyspark.sql import SparkSession, functions as f
 from pyspark.sql.types import StructType,StringType
 from Schemas.book_schemas import editionSchema
-import json
 
+load_dotenv()
+dataDirectory = os.environ.get('DATA_DIRECTORY')
 spark = SparkSession.builder.appName("Book Test").getOrCreate()
 
-df = spark.read.option("inferSchema","true").text("Data/History/ol_dump_editions_2022-10-31.txt")
+df = spark.read.option("inferSchema","true").text(dataDirectory + "/History/ol_dump_editions_2022-10-31.txt")
 
 @f.udf(returnType = StringType())
 def cleanRow(row):
@@ -15,5 +19,5 @@ def cleanRow(row):
 cleaned_df = df.select(cleanRow(f.col('value')).alias("book"))
 json_df = cleaned_df.withColumn("jsonData",f.from_json(f.col("book"),editionSchema)).select("jsonData.*")
 
-json_df.write.option("mode","overwrite").parquet("Data/Bronze/books")
+json_df.write.option("mode","overwrite").parquet(dataDirectory + "/Bronze/books")
 
--- Bronze/test_books.py
+++ Bronze/test_books.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
+import os
 from pyspark.sql import SparkSession, functions as f
 
+load_dotenv()
+dataDirectory = os.environ.get('DATA_DIRECTORY')
 spark = SparkSession.builder.appName("Book Test").getOrCreate()
 
-df = spark.read.option("inferSchema","true").parquet("Data/Bronze/books")
+df = spark.read.option("inferSchema","true").parquet(dataDirectory + "/Bronze/books")
 #df_filter = df.filter(f.col("_corrupt_record").isNotNull())
 
 df.printSchema()
