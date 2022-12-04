f8c5f555a21855ff0f0839d49f7f3fd2821915a2
andre-ls <andre.luis.e3@gmail.com>
2022-12-04 11:41:48 -0300

    Update Ratings Bronze Data Cleaning

--- Bronze/load_ratings.py
+++ Bronze/load_ratings.py
@@ -1,6 +1,8 @@
 import os
+import re
 from dotenv import load_dotenv
 from pyspark.sql import SparkSession, functions as f
+from pyspark.sql.types import StringType
 
 load_dotenv()
 dataDirectory = os.environ.get('DATA_DIRECTORY')
@@ -8,11 +10,26 @@ spark = SparkSession.builder.appName("Load Ratings").getOrCreate()
 
 df = spark.read.option("inferSchema","true").text(dataDirectory + "/History/ol_dump_ratings_2022-10-31.txt")
 
-split_df = df.withColumn('work_key',f.split(df['value'], '\t').getItem(0))\
-             .withColumn('edition_key',f.split(df['value'], '\t').getItem(1))\
-             .withColumn('rating',f.split(df['value'], '\t').getItem(2))\
-             .withColumn('date',f.split(df['value'], '\t').getItem(3))
+@f.udf(returnType = StringType())
+def cleanRow(row):
+    return re.sub("/[{}]|(\/\w*\/)","",row)
 
-split_df = split_df.drop(split_df.value)
+def cleanData(df):
+    return df.select(cleanRow(f.col('value')).alias("value"))
 
-split_df.write.option("mode","overwrite").parquet(dataDirectory + "/Bronze/ratings")
+def splitData(df):
+    split_df =  df.withColumn('work_key',f.split(df['value'], '\t').getItem(0))\
+                     .withColumn('edition_key',f.split(df['value'], '\t').getItem(1))\
+                     .withColumn('rating',f.split(df['value'], '\t').getItem(2))\
+                     .withColumn('date',f.split(df['value'], '\t').getItem(3))
+
+    return split_df.drop(split_df.value)
+
+def convertDateColumn(df):
+    return df.withColumn("date",f.to_date(f.col("date"),'yyyy-MM-dd'))
+
+df = cleanData(df)
+df = splitData(df)
+df = convertDateColumn(df)
+
+df.write.option("mode","overwrite").parquet(dataDirectory + "/Bronze/ratings")
--- Bronze/test_books.py
+++ Bronze/test_books.py
@@ -6,8 +6,8 @@ load_dotenv()
 dataDirectory = os.environ.get('DATA_DIRECTORY')
 spark = SparkSession.builder.appName("Book Test").getOrCreate()
 
-df = spark.read.option("inferSchema","true").parquet(dataDirectory + "/Bronze/authors")
-#df = spark.read.option("inferSchema","true").text(dataDirectory + "/History/ol_dump_authors_2022-10-31.txt")
+#df = spark.read.option("inferSchema","true").parquet(dataDirectory + "/Bronze/authors")
+df = spark.read.option("inferSchema","true").text(dataDirectory + "/History/ol_dump_ratings_2022-10-31.txt")
 
 df.printSchema()
 df.show(truncate=False)
