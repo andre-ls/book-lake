d9821e011eb93d01d2501001e9c118fffe1fff46
andre-ls <andre.luis.e3@gmail.com>
2022-12-09 19:55:42 -0300

    Added Book Data Loader

--- Silver/load_google_data.py
+++ Silver/load_google_data.py
@@ -1,7 +1,10 @@
 import os
 import requests
+import json
 from dotenv import load_dotenv
 from pyspark.sql import SparkSession, functions as f
+from BookDataLoader import BookDataLoader
+from Schemas.SilverSchemas import googleSchema, volumeSchema
 
 load_dotenv()
 dataDirectory = os.environ.get('DATA_DIRECTORY')
@@ -11,6 +14,10 @@ def getOpenLibraryData():
     return spark.read.parquet("DATA_DIRECTORY" + "/Bronze/books")
 
 def requestGoogleData(title,isbn):
-    return requests.get('https://www.googleapis.com/books/v1/volumes?q=' + title + '+isbn:' + isbn).json()
+    url = 'https://www.googleapis.com/books/v1/volumes?q=' + title + '+isbn:' + isbn
+    loader = BookDataLoader(url, googleSchema)
+    return loader.getData()
 
-json = requestGoogleData("O mundo de Sofia","9788571644755")
+df = requestGoogleData("O mundo de Sofia","9788571644755")
+df.show(vertical=True,truncate=False)
+df.printSchema()
