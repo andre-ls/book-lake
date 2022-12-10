4e07f469144c9d47183cdfea919c6309909750cc
andre-ls <andre.luis.e3@gmail.com>
2022-12-10 19:12:55 -0300

    Added Iteration to Google Data Extractor

--- Bronze/test_books.py
+++ Bronze/test_books.py
@@ -6,11 +6,16 @@ load_dotenv()
 dataDirectory = os.environ.get('DATA_DIRECTORY')
 spark = SparkSession.builder.appName("Book Test").getOrCreate()
 
-#df = spark.read.option("inferSchema","true").parquet(dataDirectory + "/Bronze/authors")
-df = spark.read.option("inferSchema","true").text(dataDirectory + "/History/ol_dump_ratings_2022-10-31.txt")
+df = spark.read.option("inferSchema","true").parquet(dataDirectory + "/Bronze/books")
+#df = spark.read.option("inferSchema","true").text(dataDirectory + "/History/ol_dump_ratings_2022-10-31.txt")
 
-df.printSchema()
-df.show(truncate=False)
-print("Total Rows: " + str(df.count()))
-df.select([f.count(f.when(f.col(c).isNull(), c)).alias(c) for c in df.columns]).show(vertical=True,truncate=False)
+
+df = df.withColumn("isbn",f.when(df.isbn_13.isNotNull(),df.isbn_13)\
+                             .otherwise(df.isbn_10))
+df.foreach(lambda x: print(x.isbn[0]))
+
+#df.printSchema()
+#df.show(truncate=False)
+#print("Total Rows: " + str(df.count()))
+#df.select([f.count(f.when(f.col(c).isNull(), c)).alias(c) for c in df.columns]).show(vertical=True,truncate=False)
 #print(df.where(df.isbn_10.isNull() & df.isbn_13.isNull()).count())
--- Silver/load_google_data.py
+++ Silver/load_google_data.py
@@ -10,14 +10,29 @@ load_dotenv()
 dataDirectory = os.environ.get('DATA_DIRECTORY')
 spark = SparkSession.builder.appName("Google Books Data Load").getOrCreate()
 
-def getOpenLibraryData():
-    return spark.read.parquet("DATA_DIRECTORY" + "/Bronze/books")
+def getBookReferences():
+    bookReferences = spark.read.parquet(dataDirectory + "/Bronze/books")
+    bookReferences = bookReferences.withColumn("isbn",f.when(bookReferences.isbn_13.isNotNull(),df.isbn_13)\
+                             .otherwise(df.isbn_10))
+    return bookReferences.select("title","isbn")
 
 def requestGoogleData(title,isbn):
     url = 'https://www.googleapis.com/books/v1/volumes?q=' + title + '+isbn:' + isbn
     loader = BookDataLoader(url, googleSchema)
     return loader.getData()
 
-df = requestGoogleData("O mundo de Sofia","9788571644755")
-df.show(vertical=True,truncate=False)
-df.printSchema()
+def saveData(data):
+    bookData = data.withColumn("book",f.explode("items")).select("book.*")
+    bookData.write.mode("append").parquet("DATA_DIRECTORY + /Silver/googleBooks")
+
+def getBookData(book):
+    data = requestGoogleData(book.title,book.isbn[0])
+    saveData(data)
+
+def getGoogleData(bookReferences):
+    bookReferences.foreach(lambda book: getBookData(book))
+
+#df = requestGoogleData("O mundo de Sofia","9788571644755")
+
+bookReferences = getBookReferences()
+getGoogleData(bookReferences)
